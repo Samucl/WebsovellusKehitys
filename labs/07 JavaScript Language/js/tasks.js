667761f2861a506d8ab99e51cffcf0b99a2933ff
Samuel <slaisaar00@gmail.com>
2021-03-21 17:18:38 +0200

    07 JavaScript Language valmis

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -38,6 +38,25 @@ function addTask() {
     
 } // End of addTask() function.
 
+function removeDuplicates() {
+    var uniqueTasks = [...new Set(tasks)];
+    tasks = uniqueTasks;
+
+    // Reference to where the output goes:
+    var output = document.getElementById('output');
+
+    // For the output:
+    var message = '';
+
+        // Update the page:
+        message = '<h2>To-Do</h2><ol>';
+        for (var i = 0, count = tasks.length; i < count; i++) {
+            message += '<li>' + tasks[i] + '</li>';
+        }
+        message += '</ol>';
+        output.innerHTML = message;
+}
+
 // Initial setup:
 function init() {
     'use strict';
